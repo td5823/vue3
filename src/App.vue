e67365571f86d1f5fd5179b5b7d32cb57dfbe21a
tianzhengdong <582361831@qq.com>
2023-12-16 17:07:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>App组件</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const str = '我爱你祖国'
+
+console.log('123123')
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
