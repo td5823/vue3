719225df70c33f8daf5b61152206954db952674d
tianzhengdong <582361831@qq.com>
2023-12-24 11:28:35 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h1>App组件</h1>
+    <el-button :icon="Plus">anniu</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import { Plus } from '@element-plus/icons-vue'
 // const str = '我爱你祖国'
 
 console.log('123123')
